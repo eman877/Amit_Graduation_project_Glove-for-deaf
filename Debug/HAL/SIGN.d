653eb14b4a6f47d78c057dbf02a05f2a0e12613d
Shereen Reda <85253510+shereenreda1@users.noreply.github.com>
2023-02-15 20:20:00 +0200

    Add files via upload

---
+++ Debug/HAL/SIGN.d
@@ -0,0 +1,55 @@
+HAL/SIGN.d HAL/SIGN.o: ../HAL/SIGN.c \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32a.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../config.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../MCAL/I_O.h ../MCAL/ADC.h ../HAL/LCD.h ../HAL/Sign.h
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32a.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../config.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../MCAL/I_O.h:
+
+../MCAL/ADC.h:
+
+../HAL/LCD.h:
+
+../HAL/Sign.h:
---
+++ Debug/MCAL/ADC.d
@@ -0,0 +1,50 @@
+MCAL/ADC.d MCAL/ADC.o: ../MCAL/ADC.c ../config.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../MCAL/I_O.h ../MCAL/ADC.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ D:\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32a.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+../config.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../MCAL/I_O.h:
+
+../MCAL/ADC.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+D:\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom32a.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
